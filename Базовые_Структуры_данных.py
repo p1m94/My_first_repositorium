4f3b98d21b91668066ccf787991e7a68e4bbed96
p1m94 <pimenov_v@mail.ru>
2024-10-03 20:25:12 +0700

    Add files via upload

---
+++ Базовые_Структуры_данных.py
@@ -0,0 +1,22 @@
+# Задача №1
+print ('1st program')
+print ((9**0.5)*5)
+# Задача №2
+print ('2nd program')
+print( 9.99 > 9.98 and 1000 != 1000.1)
+# Задача №3
+print ('3rd program')
+a = (2*2+2)
+print (a)
+b= (2*(2+2))
+print (b)
+print (a ==  b)
+# Задача №4
+print ('4th program')
+c = ('123.456')
+print(c[4])
+c = float(c) # Преобразование строки в дробное число
+c1 = (c*10)
+print (c1)
+c1 = int(c1)
+print (c1)
